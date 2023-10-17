40ee82dfd7e8db0472bc43587851101dcdff4a60
junitan <junitan@tencent.com>
2023-10-17 11:28:08 +0800

    fix:优化代码

--- stringx/format.go
+++ stringx/format.go
@@ -1,21 +1,22 @@
 package stringx
 
 import (
-	"encoding/json"
 	"strings"
 
+	jsonIterator "github.com/json-iterator/go"
 	"github.com/valyala/fasttemplate"
 )
 
-// Format ...
-func Format(s, start, end string, m map[string]any) (string, error) {
-	newM := make(map[string]any, len(m))
+// Format 格式化字符串
+func Format(s, start, end string, m map[string]interface{}) (string, error) {
+	newM := make(map[string]interface{}, len(m))
+	json := jsonIterator.ConfigCompatibleWithStandardLibrary // 创建jsonIterator实例
 	for k, v := range m {
-		b, err := json.Marshal(v)
+		b, err := json.MarshalToString(v)
 		if err != nil {
 			return "", err
 		}
-		newM[k] = strings.ReplaceAll(string(b), "\"", "")
+		newM[k] = strings.Trim(b, "\"")
 	}
 	value := fasttemplate.New(s, start, end).ExecuteString(newM)
 	return value, nil
--- stringx/go.mod
+++ stringx/go.mod
@@ -4,7 +4,12 @@ go 1.19
 
 require (
 	github.com/gofrs/uuid v4.4.0+incompatible
+	github.com/json-iterator/go v1.1.12
 	github.com/valyala/fasttemplate v1.2.2
 )
 
-require github.com/valyala/bytebufferpool v1.0.0 // indirect
+require (
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+)
