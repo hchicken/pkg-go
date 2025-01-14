e72f30cddaddd18dc6e147735cc94c8708785ee4
junitan <junitan@tencent.com>
2025-01-14 11:26:10 +0800

    feature: 支持路径数据的获取

--- ginx/binding/binding.go
+++ ginx/binding/binding.go
@@ -1,6 +1,9 @@
 package binding
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 	"github.com/hchicken/pkg-go/ginx/response"
 	"github.com/pkg/errors"
@@ -24,4 +27,28 @@ func ShouldBindJSON(c *gin.Context, d interface{}) error {
 func ShouldBindQuery(c *gin.Context, d interface{}) error {
 	err := c.ShouldBindQuery(d)
 	return handleError(c, err, "参数认证失败")
+}
+
+// ShouldBindPath 支持路径数据获取
+func ShouldBindPath[T string | int](c *gin.Context, paramName string, value *T) error {
+	paramValue := c.Param(paramName)
+	if paramValue == "" {
+		return handleError(c, fmt.Errorf("%v is null", paramName), "参数认证失败")
+	}
+
+	// 通过数据类型来判断
+	switch v := any(value).(type) {
+	case *int:
+		intValue, err := strconv.Atoi(paramValue)
+		if err != nil {
+			return handleError(c, fmt.Errorf("failed to parse %v as int: %v", paramName, err), "参数认证失败")
+		}
+		*v = intValue
+	case *string:
+		*v = paramValue
+	default:
+		return handleError(c, fmt.Errorf("unsupported type for %v", paramName), "参数认证失败")
+	}
+
+	return nil
 }
